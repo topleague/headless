488709eed08c5edafe477545ba033df4d98dfcc0
Susanta Sahoo <topleague@users.noreply.github.com>
2026-08-20 20:33:45 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,7 @@
 import { defineConfig } from 'astro/config';
+import cloudflare from '@astrojs/cloudflare';
 
-// https://astro.build/config
-import node from "@astrojs/node";
-
-// https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  })
+  adapter: cloudflare()
 });
